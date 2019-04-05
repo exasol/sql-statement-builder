97e135be5fd32b4932a1b30543023b932e63a88c
Anastasiia Sergienko <anastasiia.sergienko@exasol.com>
2019-04-05 13:52:20 +0200

    #43: implemented create schema logic

--- src/main/java/com/exasol/sql/TableValuesVisitor.java
+++ src/main/java/com/exasol/sql/TableValuesVisitor.java
@@ -3,7 +3,9 @@ package com.exasol.sql;
 import com.exasol.sql.dql.ValueTable;
 import com.exasol.sql.dql.ValueTableRow;
 
-public interface TableValuesVisitor extends SqlStatementVisitor {
+public interface TableValuesVisitor {
+    public void visit(Table table);
+
     public void visit(Field field);
 
     public void visit(ValueTable valueTable);
