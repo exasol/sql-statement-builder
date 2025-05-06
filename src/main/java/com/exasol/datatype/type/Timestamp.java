60bb22a3c860207268e71dff83bd91a83794210e
Antoni Reus <antoni.reus@exasol.com>
2025-05-06 13:16:11 +0200

    add precision to Timestamp types with default constructors for backwards compatibility

--- src/main/java/com/exasol/datatype/type/Timestamp.java
+++ src/main/java/com/exasol/datatype/type/Timestamp.java
@@ -6,7 +6,36 @@ import com.exasol.sql.ColumnDefinitionVisitor;
  * This class implements the SQl data type timestamp
  */
 public class Timestamp implements DataType {
+    private static final int DEFAULT_PRECISION = 3;
     private static final String NAME = "TIMESTAMP";
+
+    private final int precision;
+
+    /**
+     * Create a new instance of a {@link Timestamp} data type
+     *
+     * @param precision fractional seconds precision
+     */
+    public Timestamp(final int precision) {
+        validatePrecision(precision);
+        this.precision = precision;
+    }
+
+    /**
+     * Create a new instance of a {@link Timestamp} data type with the default fractional
+     * seconds precision (3, i.e. millisecond precision)
+     *
+     */
+    public Timestamp() {
+        this(DEFAULT_PRECISION);
+    }
+
+    /**
+     * @return precision value
+     */
+    public int getPrecision() {
+        return this.precision;
+    }
 
     @Override
     public String getName() {
@@ -16,5 +45,11 @@ public class Timestamp implements DataType {
     @Override
     public void accept(final ColumnDefinitionVisitor visitor) {
         visitor.visit(this);
+    }
+
+    private void validatePrecision(final int precision) {
+        if (precision < 0 || precision > 9) {
+            throw new IllegalArgumentException("Precision must be a number between 0 and 9.");
+        }
     }
 }
--- src/main/java/com/exasol/datatype/type/TimestampWithLocalTimezone.java
+++ src/main/java/com/exasol/datatype/type/TimestampWithLocalTimezone.java
@@ -6,7 +6,36 @@ import com.exasol.sql.ColumnDefinitionVisitor;
  * This class implements the Exasol-proprietary data type timestamp with local timezone data
  */
 public class TimestampWithLocalTimezone implements DataType {
+    private static final int DEFAULT_PRECISION = 3;
     private static final String NAME = "TIMESTAMP WITH LOCAL TIME ZONE";
+
+    private final int precision;
+
+    /**
+     * Create a new instance of a {@link TimestampWithLocalTimezone} data type
+     *
+     * @param precision fractional seconds precision
+     */
+    public TimestampWithLocalTimezone(final int precision) {
+        validatePrecision(precision);
+        this.precision = precision;
+    }
+
+    /**
+     * Create a new instance of a {@link TimestampWithLocalTimezone} data type with the default fractional
+     * seconds precision (3, i.e. millisecond precision)
+     *
+     */
+    public TimestampWithLocalTimezone() {
+        this(DEFAULT_PRECISION);
+    }
+
+    /**
+     * @return precision value
+     */
+    public int getPrecision() {
+        return this.precision;
+    }
 
     @Override
     public String getName() {
@@ -16,5 +45,11 @@ public class TimestampWithLocalTimezone implements DataType {
     @Override
     public void accept(final ColumnDefinitionVisitor visitor) {
         visitor.visit(this);
+    }
+
+    private void validatePrecision(final int precision) {
+        if (precision < 0 || precision > 9) {
+            throw new IllegalArgumentException("Precision must be a number between 0 and 9.");
+        }
     }
 }
